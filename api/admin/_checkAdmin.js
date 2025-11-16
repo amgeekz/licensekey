7e76979f7eeab81030f6848dd36fac7900e80de2
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-11-16 19:26:34 +0700

    Update _checkAdmin.js

--- api/admin/_checkAdmin.js
+++ api/admin/_checkAdmin.js
@@ -1,9 +1,6 @@
-// api/admin/_checkAdmin.js
-function isAdmin(req) {
-  const keyFromHeader = req.headers["x-admin-key"] || req.headers["X-Admin-Key"];
-  const ownerPassword = process.env.OWNER_PASSWORD;
-  if (!ownerPassword) return false;
-  return keyFromHeader === ownerPassword;
-}
+export default async function checkAdmin(key) {
+  
+  const realKey = process.env.ADMIN_KEY;
 
-module.exports = { isAdmin };
+  return key && key === realKey;
+}
