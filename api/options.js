5f0c540fa3f76dca5073a8a7ad1a061e461861d7
amgeekz <79773348+amgeekz@users.noreply.github.com>
2025-11-18 11:32:10 +0700

    Add files via upload

---
+++ api/options.js
@@ -0,0 +1,7 @@
+// api/options.js - Handle OPTIONS request
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Admin-Key');
+  res.status(200).end();
+}
